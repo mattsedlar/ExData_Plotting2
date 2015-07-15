d17e4fac5f027b9a0b95446371c995c3817a17a2
mattsedlar <sedlarm@gmail.com>
2015-07-15 10:35:32 -0700

    Plot 1 finished

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,18 @@
-source("get_data.R")
+library(dplyr)
 
+# reading data files
+
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+# Grouping df by year and summing emissions
 plot1 <- NEI %>% group_by(year) %>% summarize('total emissions' = sum(Emissions))
 
-with(plot1, plot(`total emissions` ~ year,
+# plot
+with(plot1, boxplot(`total emissions` ~ year,
                  ylab="Total PM2.5 Emissions",
-                 xlab="Year"))
+                 xlab="Year",
+                 main="Total PM2.5 Emissions in the U.S. by Year"))
+
+dev.copy(png,"Plot1.png", width=480, height=480)
+dev.off()
--- get_data.R
+++
@@ -1,6 +0,0 @@
-library(dplyr)
-
-# reading data files
-
-NEI <- readRDS("data/summarySCC_PM25.rds")
-SCC <- readRDS("data/Source_Classification_Code.rds")

f9d5752ddd64ecb280048d3f6dc58dc4395dfc14
mattsedlar <sedlarm@gmail.com>
2015-07-22 11:56:05 -0700

    Plot 4 ready

--- Plot4.R
+++ Plot4.R
@@ -12,5 +12,22 @@ scc_df <- scc_df[grep("Coal", scc_df$SCC.Level.Three),]
 # grab the numbers for coal-combustion sources
 scc_nums <- unique(scc_df$SCC)
 
-# sort NEI by year and SCC numbers
-df <- NEI %>% group_by(year,SCC) %>% summarize(`total emissions` = sum(Emissions))
+# sort NEI by year and SCC numbers, filter by coal-combustion sources
+df <- NEI %>% 
+      group_by(year,SCC) %>% 
+      summarize(`total emissions` = sum(Emissions)) %>%
+      filter(grepl(paste(scc_nums, collapse="|"),SCC))
+
+# tried to do this with mutate, instead setting year variable as factor here
+df$year <- as.factor(df$year)
+
+# plot
+# each bar has a fill representing all the emission sources
+# guide is removed because it's 80 sources and the legend looked crazy
+ggplot(df, aes(year, `total emissions`, fill=SCC)) + 
+      geom_bar(stat="identity") +
+      guides(fill=FALSE) +
+      ggtitle("Total Emissions from Coal-Combustion Sources in the U.S.")
+
+dev.copy(png, "Plot4.png", width=480, height=480)
+dev.off()
